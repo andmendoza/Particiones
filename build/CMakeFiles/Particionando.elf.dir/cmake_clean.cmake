7866b8f83b5e9b8c7c9ba8595e8786cfbcaf22a2
Andres Mendoza Z Ubuntu <andmendoza1985@gmail.com>
2022-01-07 20:53:39 +0100

    Start Project

---
+++ build/CMakeFiles/Particionando.elf.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "Particionando.bin"
+  "Particionando.map"
+  "bootloader/bootloader.bin"
+  "bootloader/bootloader.elf"
+  "bootloader/bootloader.map"
+  "config/sdkconfig.cmake"
+  "config/sdkconfig.h"
+  "project_elf_src.c"
+  "CMakeFiles/Particionando.elf.dir/project_elf_src.c.obj"
+  "Particionando.elf"
+  "Particionando.elf.pdb"
+  "project_elf_src.c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Particionando.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
